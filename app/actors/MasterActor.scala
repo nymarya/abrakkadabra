42fbf22f9fd91ae91a238382f083046e65f25acd
Mayra Azevedo <mayradazevedo@gmail.com>
2019-12-02 14:53:01 -0300

    visualiza matrix

--- app/actors/MasterActor.scala
+++ app/actors/MasterActor.scala
@@ -43,7 +43,7 @@ class MasterActor extends Actor {
     }
     case m: Matrices => sparkActor ! m
     case b: BlockMatrix => {
-      val aaa = b.toCoordinateMatrix().toRowMatrix().rows.flatMap( a => a.toArray.mkString(","))
+      val aaa = b.toCoordinateMatrix().toRowMatrix().rows.flatMap( a => a.toArray.mkString(",")).flatMap(_)
       println(aaa)
       sender().forward(aaa.toString())
     }

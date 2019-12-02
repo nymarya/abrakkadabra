e4f3a5e7bcc3a5acfe397a74f6aa4aa18151840b
Mayra Azevedo <mayradazevedo@gmail.com>
2019-12-02 04:59:30 -0300

    adiciona allow filtering

--- app/actors/DatabaseActor.scala
+++ app/actors/DatabaseActor.scala
@@ -27,7 +27,7 @@ class DatabaseActor extends Actor{
     case rows: Int => {
       println("cass")
       val keyspaceName = "abrakkadabra"
-      val stmt = new SimpleStatement(s"SELECT * FROM $keyspaceName.matrixes WHERE rows=$rows").setFetchSize(1)
+      val stmt = new SimpleStatement(s"SELECT * FROM $keyspaceName.matrixes WHERE rows=$rows limit 1 ALLOW FILTERING")
       val results = CassandraSource(stmt).runWith(Sink.seq)
 
       val d : Duration = 1.seconds

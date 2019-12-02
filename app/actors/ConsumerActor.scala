58b08eb19d2582fda2adfd39c799380a00484d56
Mayra Azevedo <mayradazevedo@gmail.com>
2019-12-01 21:19:36 -0300

    corrige importaçoes

--- app/actors/ConsumerActor.scala
+++ app/actors/ConsumerActor.scala
@@ -1,16 +1,18 @@
 package actors
 
+import akka.Done
 import akka.kafka.{ConsumerSettings, Subscriptions}
 import akka.kafka.scaladsl.Consumer
 import akka.stream.scaladsl.{Keep, Sink}
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer}
 
 import scala.concurrent.Future
-import akka.Done
 import akka.actor.{Actor, Props}
 import akka.kafka.scaladsl.Consumer.DrainingControl
 import akka.stream.ActorMaterializer
-import org.apache.kafka.clients.consumer.ConsumerConfig
+import com.typesafe.config.Config
+import messages.Kernel
+import org.apache.kafka.clients.consumer.{ ConsumerConfig}
 
 import scala.collection.immutable
 
@@ -28,16 +30,22 @@ class ConsumerActor extends Actor{
       .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
   val kafkaConsumer = consumerSettings.createKafkaConsumer()
   implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  def business( value: Array[Byte]): Future[Done] = {// ???
+  // #atMostOnce
+  // #atMostOnce
+    val k = (value.map(_.toChar)).mkString
+    sender() ! Kernel(k)
+    Future.successful(Done)
+  }
+
+
   def receive = {
     case topic:  String => {
       val control: DrainingControl[immutable.Seq[Done]] =
         Consumer
           .atMostOnceSource(consumerSettings, Subscriptions.topics(topic))
-          .mapAsync(1)(record =>{
-            sender() ! record.value()
-
-            Future.successful(Done)
-          })
+          .mapAsync(1)(record => business(record.value()))
           .toMat(Sink.seq)(Keep.both)
           .mapMaterializedValue(DrainingControl.apply)
           .run()
